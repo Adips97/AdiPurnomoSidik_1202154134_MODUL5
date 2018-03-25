bbdf1706cffe84f45b8f256732e2d6e611153c6c
Adi Purnomo <adipurnomosidik97@gmail.com>
2018-03-25 22:15:07 +0700

    Push pertama kali

---
+++ app/src/main/java/android/example/com/adipurnomosidik_1202154134_modul5/AddTodo.java
@@ -0,0 +1,34 @@
+package android.example.com.adipurnomosidik_1202154134_modul5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddTodo extends AppCompatActivity {
+
+    //Deklarasi Komponen yang akan digunakan
+    private EditText etName, etDesc, etPriority;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+
+        //Inisialisasi Komponen View
+        etName = (EditText)findViewById(R.id.etName);
+        etDesc = (EditText)findViewById(R.id.etDesc);
+        etPriority = (EditText)findViewById(R.id.etPriority);
+    }
+
+
+    //menambahkan fungsi ketika menambahkan file database
+    public void addTodo(View view) {
+        Intent intent=new Intent();
+        intent.putExtra("name",etName.getText().toString());
+        intent.putExtra("desc",etDesc.getText().toString());
+        intent.putExtra("priority",etPriority.getText().toString());
+        setResult(1,intent);
+        finish();//finishing activity
+    }
+}
